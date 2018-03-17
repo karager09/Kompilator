961e52cd078c9f1f2cca9b0f9234033e9d97eab9
Piotr <karager009@gmail.com>
2018-03-17 21:19:19 +0100

    third

--- src/agh/edu/pl/Main.java
+++ src/agh/edu/pl/Main.java
@@ -10,30 +10,68 @@ public class Main {
     static FileReader in;
     static FileWriter out;
 
+    static void identyfikatory(String ciag) throws IOException {
+        switch(ciag){
+            case "if": out.write("<symif>"+ciag+"</symif>"); break;
+            case "while": out.write("<symwhile>"+ciag+"</symwhile>"); break;
+            case "for": out.write("<symfor>"+ciag+"</symfor>"); break;
+            case "": out.write("<symif>"+ciag+"</symif>"); break;
+            default: out.write("<identyfikator>"+ciag+"</identyfikator>");
+
+
+        }
+
+
+    }
+
+
     static void sprawdzaj(char znak) throws IOException {
         String ciag = ""+znak;
-        System.out.println(znak);
+
+        if(znak == ' ' || znak == '\n' || znak == '\t' || znak == '\0' || (int)znak < 30){
+            if(znak != ' ' && znak != '\t')out.write("\n"+"<br>");
+            int liczba = in.read();
+            if(liczba > 0) sprawdzaj((char)in.read());
+        }
+        //System.out.println(znak);
         if( znak >= '0' && znak <= '9'){
             int liczba;
             char litera;
-            while((liczba = in.read()) != -1){
+            while((liczba = in.read()) > 0){
                 litera = (char) liczba;
-                System.out.println(litera);
+                //System.out.println(litera);
                 if(litera < '0' || litera > '9'){
 
                     out.write("<liczba>"+ciag+"</liczba>");
 
                     sprawdzaj(litera);
+                    return;
+                }
+                ciag +=litera;
+            }
+            out.write("<liczba>"+ciag+"</liczba>");
+        }
+        else if( (znak >= 'a' && znak <= 'z') || (znak >= 'A' && znak <= 'Z')){
+            //System.out.println(znak);
+            int liczba;
+            char litera;
+            while((liczba = in.read()) > 0){
+                litera = (char) liczba;
+                //System.out.println(litera);
+                if(!((litera >= 'a' && litera <= 'z') || (litera >= 'A' && litera <= 'Z'))){
+
+                    identyfikatory(ciag);
+                    sprawdzaj(litera);
+                    return;
                 }
                 ciag +=litera;
 
-
             }
-
-            //out.write("<liczba>"+ciag+"</liczba>");
-
+            identyfikatory(ciag);
 
         }
+
+
 
 
 
@@ -50,7 +88,7 @@ public class Main {
 
             int liczba;
             in.read();
-//
+
 //            while((liczba = in.read()) != -1){
 //                char litera = (char) liczba;
 //                System.out.println(litera);
@@ -59,7 +97,7 @@ public class Main {
 
             if((liczba = in.read()) != -1) sprawdzaj((char)liczba);
 
-            out.write("</program>");
+            out.write("\n<br></program>");
 
             out.close();
             in.close();
