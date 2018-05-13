36cbb3518266c1f8f2f48d2365d43e1f0adfaa9a
Piotr <karager009@gmail.com>
2018-05-13 12:46:45 +0200

    writing listener

---
+++ probne_c.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <stdlib.h>
+
+#define INT 0
+#define DOUBLE 1
+#define STRING 2
+#define TABLE 3
+
+using namespace std;
+
+
+class Variable{
+    public:
+    short type;
+    void * value;
+
+/*
+    Variable(short type){
+        this->type = type;
+        if(type == 0){
+            value = malloc(sizeof(int));
+            *((int*)value) = 5;
+        }
+    }
+*/
+
+
+    Variable(int rValue){
+        this->type = INT;
+
+            value = malloc(sizeof(int));
+            *((int*)value) = rValue;
+    }
+
+    Variable operator+(Variable v){
+        if(this->type == INT){
+            return Variable(*((int*)this->value) + *((int*)v.value));
+        } else if(this->type == DOUBLE){
+            return Variable(*((double*)this->value) + *((double*)v.value));
+        }
+    }
+
+
+};
+
+
+Variable jakasFunkcja(Variable v1, Variable v2){
+
+    Variable result = Variable(*((int*)v1.value) + *((int*)v2.value));
+    return result;
+
+}
+
+int main()
+{
+
+    Variable v = Variable(8);
+    cout << v.type<< ", "<< *((int*)v.value)<< endl;
+
+    cout << v.type<< ", "<< *((int*)(jakasFunkcja(v,v)).value)<< endl;
+
+    cout << v.type<< ", "<< *((int*)(v+1).value) << endl;
+
+    return 0;
+}
